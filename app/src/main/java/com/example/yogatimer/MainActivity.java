1bffa4905ad790df49be22bb3389fc98a615ee63
John <jb8540@gmail.com>
2022-08-17 22:00:53 +0100

    in MainActivity.java:- added spaces in editTexts in setup menu, so its easier to change values. then had to remove spaces from strings to convert back to integers.

--- app/src/main/java/com/example/yogatimer/MainActivity.java
+++ app/src/main/java/com/example/yogatimer/MainActivity.java
@@ -126,12 +126,16 @@ public class MainActivity extends Activity {
     public void onClickSetup(View view)
     {
          if( editLoop1.getVisibility()==View.VISIBLE)
-        {  // click save button
-            loop1 = Integer.parseInt(editLoop1.getText().toString());
-            loop2 = Integer.parseInt(editLoop2.getText().toString());
-            pauseDelay = Integer.parseInt(editPause.getText().toString());
+        {  //  save button has been clicked:- so update loop1,loop2,pauseDelay, change button label to SETUP
+            // convert to integer - get String - remove spaces
+            pauseDelay = Integer.parseInt(editPause.getText().toString()
+                                    .replace(" ", ""));// spaces removed
             pauseDelay = Math.abs(pauseDelay);
             loopTimer = -pauseDelay;
+            loop1 = Integer.parseInt(editLoop1.getText().toString()
+                                            .replace(" ", ""));// spaces removed
+            loop2 = Integer.parseInt(editLoop2.getText().toString()
+                    .replace(" ", ""));// spaces removed);
             saveData();
            // Close keyboard
             ((InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(editPause.getWindowToken(), 0);
@@ -147,16 +151,17 @@ public class MainActivity extends Activity {
             loop1Button.setText("loop " + Integer.toString(loop1) );//set the text on button
             loop2Button.setText("loop " + Integer.toString(loop2) );//set the text on button
         }else
-        {
+        { // setup button has been clicked:- change button label to SAVE, open edit texts
             setupButton.setText("Save  " );//set the text on button
-            editLoop1.setText(Integer.toString(loop1));
-            editLoop2.setText(Integer.toString(loop2));
-            editPause.setText(Integer.toString(pauseDelay));
+            editLoop1.setText("   "+Integer.toString(loop1));// add spaces for easy selectin
+            editLoop2.setText("   "+Integer.toString(loop2));// add spaces for easy selectin
+            editPause.setText("   "+Integer.toString(pauseDelay));// add spaces for easy selectin
             labelPause.setVisibility(View.VISIBLE);
             editPause.setVisibility(View.VISIBLE);
             editLoop1.setVisibility(View.VISIBLE);
             labelLoop1.setVisibility(View.VISIBLE);
             editLoop2.setVisibility(View.VISIBLE);
+            editLoop2.setSelection(0);
             labelLoop2.setVisibility(View.VISIBLE);
             // Open keyboard
             ((InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(editPause, InputMethodManager.SHOW_FORCED);
